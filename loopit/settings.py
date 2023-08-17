9805ed427d6282973b20ef9f4e623c656b835a3d
Birger <birger.oesterberg@gmail.com>
2023-08-17 21:18:14 +0200

    update allowed hosts for deployment

--- loopit/settings.py
+++ loopit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['loopit.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['loopit-561b13093461.herokuapp.com', 'localhost']
 
 
 # Application definition
