e878e7939df701b993cedff5514dddfb2663508a
Birger <birger.oesterberg@gmail.com>
2023-08-20 23:43:00 +0200

    add post_detail.html to view posts, install allauth

--- community/views.py
+++ community/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -8,3 +8,17 @@ class PostList(generic.ListView):
     queryset = Post.objects.order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 10
+
+
+class PostDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = Post.objects.get(slug=slug)
+        comments = post.comments.order_by('-created_on')
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+            },
+        )
