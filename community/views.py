5514db2c2b6ad829c24e08af0c79906320830303
Birger <birger.oesterberg@gmail.com>
2023-08-21 22:15:06 +0200

    add crispy forms comment functionality, fix designs on "account" files

--- community/views.py
+++ community/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -20,5 +21,31 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        post = Post.objects.get(slug=slug)
+        comments = post.comments.order_by('-created_on')
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "comment_form": CommentForm()
             },
         )
