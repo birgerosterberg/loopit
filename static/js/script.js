96da26e5552c6ac9b36036c8eb11061340a9789b
Birger <birger.oesterberg@gmail.com>
2023-08-30 22:29:49 +0200

    redesign category links, js, css

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,17 @@ setTimeout(function () {
   let alert = new bootstrap.Alert(messages);
   alert.close();
 }, 2500);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const category = urlParams.get("category") || "All";
+
+  document.querySelectorAll(".category-link").forEach((link) => {
+    const linkCategory = link.textContent;
+    if (linkCategory === category) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
